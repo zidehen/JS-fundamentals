00b1707eede6365099e2c938b58b24d1e7002934
zidehen <zoeidehen@gmail.com>
2022-01-07 08:57:45 +0000

    implemented addItem to basket

--- shopping-basket/shopping-basket.js
+++ shopping-basket/shopping-basket.js
@@ -1,17 +1,24 @@
-
+const Candy = require('./candy')
 class ShoppingBasket {
   
   constructor() {
     this.totalPrice = 0
+    this.items = []
   } 
 
   getTotalPrice() {
-    return this.totalPrice
+    return this.totalPrice;
+  }
+
+  addItem(candy) {
+    return this.items.push(candy);
+
   }
 }
 
-
 module.exports = ShoppingBasket;
 
-// const basket = new ShoppingBasket();
-// console.log(basket.getTotalPrice())
+const basket = new ShoppingBasket();
+// console.log(basket.getTotalPrice());
+console.log(basket.addItem('Mars'));
+console.log(basket.items)
--- shopping-basket/shopping-basket.test.js
+++ shopping-basket/shopping-basket.test.js
@@ -1,10 +1,21 @@
 const ShoppingBasket = require('./shopping-basket')
+const Candy = require('./candy')
 
 describe('ShoppingBasket class', () => {
 
-    it('returns the total price of basket', () => {
-      const basket = new ShoppingBasket()
-      expect(basket.getTotalPrice()).toEqual(0);
-    });
+  it('returns the total price of basket', () => {
+    const basket = new ShoppingBasket();
+    expect(basket.getTotalPrice()).toEqual(0);
+  });
+
+  it('adds items to basket', () => {
+    const candyDouble = { name: 'Haribo', price: 1.99 };
+    const basket = new ShoppingBasket();
+    basket.items = []
+    basket.addItem(candyDouble);
+    expect(basket.items[0].name).toEqual('Haribo');
+  });
 
 })
+
+
