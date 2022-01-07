de4fa27b0c54b2ec82e496b2c352330d1060b971
zidehen <zoeidehen@gmail.com>
2022-01-07 15:11:33 +0000

    implemented getTotalPrice() TDD and refactored

--- shopping-basket/shopping-basket.js
+++ shopping-basket/shopping-basket.js
@@ -2,23 +2,29 @@ const Candy = require('./candy')
 class ShoppingBasket {
   
   constructor() {
-    this.totalPrice = 0
     this.items = []
   } 
-
-  getTotalPrice() {
-    return this.totalPrice;
-  }
-
+  
   addItem(candy) {
-    return this.items.push(candy);
+    this.items.push(candy);
+    return `${candy.name} added to basket`;
 
+  }
+  getTotalPrice() {
+    let totalPrice = 0
+    this.items.map((item) => {
+      totalPrice += item.price;
+    })
+    return totalPrice
   }
 }
 
 module.exports = ShoppingBasket;
 
+const candy1 = new Candy('Oreo', 3.99)
+const candy2 = new Candy('Skittles', 3.99)
 const basket = new ShoppingBasket();
-// console.log(basket.getTotalPrice());
-console.log(basket.addItem('Mars'));
-console.log(basket.items)
+
+console.log(basket.addItem(candy1));
+console.log(basket.addItem(candy2));
+console.log(basket.getTotalPrice());
--- shopping-basket/shopping-basket.test.js
+++ shopping-basket/shopping-basket.test.js
@@ -3,18 +3,28 @@ const Candy = require('./candy')
 
 describe('ShoppingBasket class', () => {
 
-  it('returns the total price of basket', () => {
-    const basket = new ShoppingBasket();
+  const basket = new ShoppingBasket();
+  let candyDouble1 = { name: 'Haribo', price: 1.99 };
+  let candyDouble2 = new Candy('Skittles', 2.99)
+
+  it('returns price of 0 when basket empty', () => {
     expect(basket.getTotalPrice()).toEqual(0);
   });
-
+  
   it('adds items to basket', () => {
-    const candyDouble = { name: 'Haribo', price: 1.99 };
-    const basket = new ShoppingBasket();
     basket.items = []
-    basket.addItem(candyDouble);
+    basket.addItem(candyDouble1);
     expect(basket.items[0].name).toEqual('Haribo');
   });
+
+  it('returns the total price of all items in basket', () => {
+    basket.items = []
+    basket.addItem(candyDouble1);
+    basket.addItem(candyDouble2);
+    expect(basket.getTotalPrice()).toEqual(4.98);
+  });
+
+  
 
 })
 
